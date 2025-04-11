6c01654d7cbe1bf8cad22acbc88816746cc2f3e6
Hamza <hamzabenbrahim6060@gmail.com>
2025-04-11 13:15:48 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         callMe();
         printMe();
+        System.out.println(addSum(3,100));
 
         }
         public static void callMe(){
@@ -12,6 +13,13 @@ public class Main {
         }
         public static void printMe(){
             System.out.println("GJW9");
+        }
+        public static int addSum (int b, int c){
+            int a = b + c;
+           return a;
+
+
+
         }
         
 
